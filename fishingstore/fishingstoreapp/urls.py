c4ecffa1f7655da2ca0d69492c40471fa6e62ca3
Kamil Pasterczyk <yiome@outlook.com>
2022-03-26 17:04:52 +0100

    added some mockup endpoints

--- fishingstore/fishingstoreapp/urls.py
+++ fishingstore/fishingstoreapp/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add_product/', views.add_product, name='add_product'),
+    path('get_products/', views.get_products, name='get_products'),
+    path('add_mockup_products/', views.add_mockup_products, name='add_mockup_products'),
+    path('create_table/', views.create_table, name='create_table'),
 ]
