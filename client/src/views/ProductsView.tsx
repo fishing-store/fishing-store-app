d07c3bb68a1d533269f87e390404ecd0d097392e
AdriannaUrbanska <ada.urbanska@wp.pl>
2022-04-19 23:30:51 +0200

    FS-63 created a button to direct to add product page

--- client/src/views/ProductsView.tsx
+++ client/src/views/ProductsView.tsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import ROUTES from "../utils/ROUTES.json";
+
 const ProductsView = () => {
   return (
     <div>
-      <strong>Products View</strong>
+	  <Link to={ROUTES.addproduct}>
+		  <Button variant="primary">
+				Add new product
+		  </Button>
+	  </Link>
     </div>
   );
 };
