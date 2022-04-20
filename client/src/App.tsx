d28fa0d479ef31e4c5f2496b1679b99c8656ef0e
DESKTOP-8KLMAJ0\Asia Marcinkowska <asiamarcinkowska5@gmail.com>
2022-04-20 16:20:36 +0200

    Added product details

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import {
   AddProductView,
   DeliveryView,
   AboutUsView,
+  ProductDetailsView
 } from "./views";
 import Navbar from "./components/Navbar";
 import ROUTES from "./utils/ROUTES.json";
@@ -80,6 +81,7 @@ const App = () => {
                       element={<DeliveryView />}
                     ></Route>
                     <Route path={ROUTES.info} element={<AboutUsView />}></Route>
+                    <Route path={ROUTES.product} element={<ProductDetailsView id={1} />}></Route>
                   </Routes>
                 </DeliveryProvider>
               </Box>
