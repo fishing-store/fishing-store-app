157af527a74ab4a35dc54d209f2adbccfed9e178
Xmiryu <61467204+Xmiryu@users.noreply.github.com>
2022-05-02 19:30:32 +0200

    Add backend and frontend for login

--- fishingstore/api/serializers.py
+++ fishingstore/api/serializers.py
@@ -57,3 +57,30 @@ class RegisterSerializer(serializers.ModelSerializer):
         user.save()
 
         return user
+
+
+class LoginSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=255)
+    password = serializers.CharField(max_length=128, write_only=True)
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    def validate(self, data):
+        username = data.get("username", None)
+        password = data.get("password", None)
+        user = authenticate(username=username, password=password)
+        if user is None:
+            raise serializers.ValidationError(
+                'A user with this username and password not found.'
+            )
+        try:
+            payload = JWT_PAYLOAD_HANDLER(user)
+            jwt_token = JWT_ENCODE_HANDLER(payload)
+            update_last_login(None, user)
+        except User.DoesNotExist:
+            raise serializers.ValidationError(
+                'User with given username and password does not exist'
+            )
+        return {
+            'username':user.username,
+            'token': jwt_token
+        }
