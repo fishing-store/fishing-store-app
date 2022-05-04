6121e7fbe6646cbba7ee3e12bdf080429fee173e
Marek <mark-now@wp.pl>
2022-05-04 22:12:27 +0200

    fix

--- fishingstore/api/views.py
+++ fishingstore/api/views.py
@@ -187,11 +187,6 @@ class LoginView(generics.CreateAPIView):
 
 # Endpoint created for veryfing user authorization
 # Attach access token to get request
-@api_view(['GET'])
-def get_hello(request: HttpRequest):
-    content = json.dumps({'message': 'Hello, World!', 'username': request.user.username, 'is_superuser': request.user.is_superuser, 'email': request.user.email})
-    return HttpResponse(content)
-
 class HelloView(APIView):
     permission_classes = (IsAuthenticated,)
 
