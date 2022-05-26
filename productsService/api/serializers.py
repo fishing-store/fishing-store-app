88e9b610e37b0467861074d3946ff5976e75f36e
dtrybuch <dominik.trybuch@gmail.com>
2022-05-26 19:43:59 +0200

    add draft

---
+++ productsService/api/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from .models import Product, Category
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'price', 'description', 'image', 'count', 'categories')
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('name',)
---
+++ productsService/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.ProductAPIView.as_view(), name='index'),
+    path('products/', views.get_all_products, name='get_products'),
+    path('products/add', views.add_product, name='add_product'),
+    path('saveProduct/<id>', views.put_product, name='put_product'),
+    path('products/<id>', views.get_add_update_product, name='get_add_update_product'),
+    path('categories', views.get_categories, name='get_categories'),
+]
